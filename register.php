89e9c274214eb1024550914afe48a98f86eef2a5
mosesLelo <botmoise@gmail.com>
2019-02-22 18:17:01 +0100

    modification de la structure

--- register.php
+++ register.php
@@ -1,51 +1,6 @@
 <?php
-//Evite certain probleme de Hacking 
-function sanitizeFormUsername($inputText){
-
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-}
-
-//Evite certain probleme de Hacking 
-function sanitizeFormString($inputText){
-
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-
-function sanitizeFormPassword($inputText){
-
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-
-if(isset($_POST['loginButton'])){
-  
-
-}
-
-if(isset($_POST['RegisterButton'])){
-  
-   $username = sanitizeFormUsername($_POST['username']);   
-
-   $firstName = sanitizeFormString($_POST['firstName']);
-
-   $lastName = sanitizeFormString($_POST['lastName']);
-
-   $email = sanitizeFormString($_POST['email']);
-
-   $email2 = sanitizeFormString($_POST['email2']);
-
-   $password = sanitizeFormPassword($_POST['password']);
-  
-   $password2 = sanitizeFormPassword($_POST['password2']);
-
-
-    
-}
+    include("includes/handlers/register-handler.php");
+    include("includes/handlers/login-handler.php");
 
 ?>
 
