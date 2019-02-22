800e9bf78ef3472b153f632c7c77e73ffeee0a2d
mosesLelo <botmoise@gmail.com>
2019-02-22 20:47:17 +0100

    modification

---
+++ includes/classes/Constants.php
@@ -0,0 +1,16 @@
+<?php
+class Constants{
+
+    public static $passwordsDoNotMatch = "mot de passe non similaire";
+    public static $passwordDoNotAlphanumeric = "le mot de passe doit seulement contenir des chiffres et des lettres";
+    public static $passwordCharacters = "Ton mot de passe doit etre compris entre 5 et 30 characteres";
+    public static $emailInvalid = "l'email n'est pas valide";
+    public static $emailsDoNotMatch = "les emails sont différents";
+    public static $lastNameCharacters = "La taille de ton nom toi etre entre 5 et 25 characteres";
+    public static $firstNameCharacters = "La taille de ton prenom toi etre entre 5 et 25 characteres";
+    public static $usernameCharacters = "La taille de ton pseudo toi etre entre 5 et 25 characteres";
+
+}
+
+
+?>
